0a5a17b46430cae7cda54b9ad48ee17f55c14dd8
aspalvieri <alex.spalvieri@hotmail.com>
2019-07-27 17:54:44 -0400

    final push

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -2,7 +2,13 @@ import React from "react";
 
 function About() {
     return (
-        <h1>About Page</h1>
+        <div style={{padding: "10px 25px"}}>
+            <h1 style={{textAlign: "center"}}>About Page</h1>
+            <div style={{backgroundColor: "lightgray", padding: "25px"}}>
+                <h2>Alex Spalvieri <small>200403578</small></h2>
+                <p>The one and only member of Group 1! I enjoy to make and play video games. I primarily use C++ with the SDL library to make my games.</p>
+            </div>
+        </div>
     );
 }
 
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -2,7 +2,14 @@ import React from "react";
 
 function Contact() {
     return (
-        <h1>Contact Page</h1>
+        <div style={{padding: "10px 25px"}}>
+            <h1 style={{textAlign: "center"}}>Contact Page</h1>
+            <div style={{backgroundColor: "lightgray", width: "400px", padding: "25px", border: "5px solid black"}}>
+                <h2>Alex Spalvieri <small>200403578</small></h2>
+                <p><strong>Email:</strong> 200403578@student.georgianc.on.ca</p>
+                <p><strong>Phone:</strong> (416) 556-2012</p>
+            </div>
+        </div>
     );
 }
 
